30cd9b44d0be3d71660755ffe0eccb8436ea3c64
mtotodev05 <mtotodev05@gmail.com>
2018-05-03 00:08:51 +0300

    updated dependencies

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-var twit = require('twit')
+var twit = require('twitter')
 var config = require('./config')
 
 var Twitter = new twit(config)
 
 var retweet = function(){
     var params = {
-        q: '#JKL, #nodejs',
+        q: '#StateOfTheNation',
         result_type: 'recent',
         lang: 'en'
     }
-
-
+    Twitter.get('search/tweets',params,function(err,data){
+        if(!err){
+            var retweetId = data.statuses[0].id_str;
+            Twitter.post('/statuses/retweet/:id',{
+                id: retweetId
+            },function(err,response){
+                if(response){
+                    console.log('Retweeted!!!');
+                }
+                if(err){
+                    console.log('Something went wrong while processing Tweets, Status maybe a duplicate')
+                }
+            });
+        }else{
+            console.log('Error while Searching');
+        }
+    });
 }
+retweet();
+setInterval(retweet,30000)
